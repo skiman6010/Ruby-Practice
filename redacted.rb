cb081c7fa7ad3b21e3fdc805af70f5cf3a9e43f8
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2017-08-11 11:23:18 -0400

    Create redacted.rb

---
+++ redacted.rb
@@ -0,0 +1,14 @@
+puts "Text to search through: "
+text = gets.chomp
+puts "Word you want to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
+end
