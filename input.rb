3f5a56b4fcf99756e0a432a534eea0ce0521fd43
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2017-08-10 21:41:40 -0400

    Create input.rb

---
+++ input.rb
@@ -0,0 +1,13 @@
+print "What's your first name? "
+first_name = gets.chomp.capitalize!
+
+print "What's your last name? "
+last_name = gets.chomp.capitalize!
+
+print "What city are you from? "
+city = gets.chomp.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp.upcase!
+
+puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
